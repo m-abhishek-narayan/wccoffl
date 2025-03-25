5db900d38809a5a978b4839507dc022fec608479
varunvinod30 <varunvinod30@gmail.com>
2025-03-25 17:21:46 +0530

    Scriptadded

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,20 @@
+import React from 'react';
+import { useState,useEffect } from 'react';
+import ReactDOM from 'react-dom';
 function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/message") // Call backend API
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
   return (
     <header>
       <video src="public/video.mp4" loop autoPlay muted></video>
-      <h1>Open Source Website</h1>
       <div className="row">
-        <button className="btn" style={{ cursor: "pointer" }}>
-          Sign Up
-        </button>
-
-        <button className="btn" style={{ cursor: "pointer" }}>
-          Log in
-        </button>
+        <h1>{message ? message : "WCC"}</h1>
       </div>
 
       <div className="headerbg"></div>
