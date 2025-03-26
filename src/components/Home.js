6dc9bc89fc2b155642e1a1222d48a6e78e9aefd6
varunvinod30 <varunvinod30@gmail.com>
2025-03-26 19:56:52 +0530

    used railway as backend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,20 @@
+import React from 'react';
+import { useState,useEffect } from 'react';
 function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("https://wccbackend-production.up.railway.app//") 
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("Error fetching data:", err));
+  }, []);
+
   return (
     <header>
       <video src="public/video.mp4" loop autoPlay muted></video>
-      <h1>Open Source Website</h1>
       <div className="row">
-        <button className="btn" style={{ cursor: "pointer" }}>
-          Sign Up
-        </button>
-
-        <button className="btn" style={{ cursor: "pointer" }}>
-          Log in
-        </button>
+        <h1>{message ? message : "WCC"}</h1>
       </div>
 
       <div className="headerbg"></div>
