9f77e663ef9b79ce49c0f27d484311d75e20bdee
varunvinod30 <varunvinod30@gmail.com>
2025-03-29 19:13:18 +0530

    ChatUi Changed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
+import React, { useEffect, useState } from "react";
+import Home from "./components/Home"
+import Profile from "./components/Profile";
+import Gallery from "./components/Gallery";
+import Awards from "./components/Awards";
 import Chat from "./components/Chat";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer"
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <Home />
-      <Main />
-      <Footer />
-      <Chat />
-    </>
-  );
-}
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <div id="home"><Home /></div>
+            <div id="profile"><Profile /></div>
+            <div id="gallery"><Gallery /></div>
+            <div id="awards"><Awards /></div>
+            <Chat />
+            <Footer />
+        </>
+    );
+};
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,15 @@
-import Gallery from './Gallery';
-import Awards from './Awards'; 
-import Profile from './Profile';
+import Profile from "./Profile";
+import Gallery from "./Gallery";
+import Awards from "./Awards";
 
 function Main() {
   return (
-    <>
-      <main>
-        <Profile />
-        <Gallery />
-        <Awards />
-      </main>
-    </>
+    <main>
+      <Profile />
+      <Gallery />
+      <Awards />
+    </main>
   );
 }
+
 export default Main;
