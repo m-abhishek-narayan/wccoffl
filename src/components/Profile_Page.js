17112bae00e9e3b9cb79d3cb25c2b07d74447e24
m-abhishek-narayan <79897302+m-abhishek-narayan@users.noreply.github.com>
2025-04-07 22:23:24 +0530

    increased padding

--- src/components/Profile_Page.js
+++ src/components/Profile_Page.js
@@ -13,7 +13,7 @@ const Profile_Page = () => {
   );
 
   return (
-    <div className="Profile_page pt-20 bg-gray-50 min-h-screen p-4">
+    <div className="Profile_page pt-30 bg-gray-50 min-h-screen p-4">
       <div className="bg-gray-50 min-h-screen p-4">
         {/* Page Title */}
         <h1 className="text-3xl font-bold text-center mb-6">Player Profiles</h1>
