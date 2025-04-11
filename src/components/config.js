7b2e758767c0c1afa4615f282e4997a3af811e6b
varunvinod30 <varunvinod30@gmail.com>
2025-04-11 19:08:49 +0530

    bug fixed

--- src/components/config.js
+++ src/components/config.js
@@ -1,9 +1,9 @@
 const isLocal = window.location.hostname === "localhost";
 
 export const MESSAGE_API = isLocal
-  ? "http://localhost:8080/api"
+  ? "https://wccbackend.onrender.com/api"
   : "https://wccbackend.onrender.com/api";
 
 export const SOCKET_URL = isLocal
-  ? "http://localhost:8080"
+  ? "https://wccbackend.onrender.com"
   : "https://wccbackend.onrender.com";
