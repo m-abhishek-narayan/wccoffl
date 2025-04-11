9202ae48075608ce2c9912b57502b81b886a15a2
varunvinod30 <varunvinod30@gmail.com>
2025-04-11 17:45:44 +0530

    port changed

--- src/components/config.js
+++ src/components/config.js
@@ -1,9 +1,9 @@
 const isLocal = window.location.hostname === "localhost";
 
 export const MESSAGE_API = isLocal
-  ? "http://localhost:3000/api"
+  ? "http://localhost:8080/api"
   : "https://wccbackend.onrender.com/api";
 
 export const SOCKET_URL = isLocal
-  ? "http://localhost:3000"
+  ? "http://localhost:8080"
   : "https://wccbackend.onrender.com";
